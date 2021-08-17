38758b05a20b7bad5f87a5eef938380c4b75cfa9
Eva Prokhorenko <evaprokhorenko@gmail.com>
2021-08-17 00:17:48 +0000

    final commit

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,8 @@
-const assertEqual = function (actual, expected) {
-  const emo1 = String.fromCodePoint(0x1F6D1);
-  const emo2 = String.fromCodePoint(0x2705);
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`${emo2}${emo2}${emo2}Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`${emo1}${emo1}${emo1}Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`⛔️ ⛔️ ⛔️Assertion Failed: ${actual} !== ${expected}`);
   }
 
 };
--- tail.js
+++ tail.js
@@ -1,28 +1,23 @@
 
-const assertEqual = function (actual, expected) {
-  const emo1 = String.fromCodePoint(0x1F6D1);
-  const emo2 = String.fromCodePoint(0x2705);
-
+const assertEqual = function(actual, expected) {
   if (actual.length !== expected.length) {
-    console.log(`${emo1}${emo1}${emo1}Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`⛔️ ⛔️ ⛔️ Assertion Failed: ${actual} !== ${expected}`);
     return;
   }
 
   for (let i = 0; i < actual.length; i++) {
     if (actual[i] !== expected[i]) {
-      console.log(`${emo1}${emo1}${emo1}Assertion Failed: ${actual} !== ${expected}`);
+      console.log(`$⛔️ ⛔️ ⛔️Assertion Failed: ${actual} !== ${expected}`);
       return;
     }
   }
 
-  console.log(`${emo2}${emo2}${emo2}Assertion Passed: ${actual} === ${expected}`);
+  console.log(`✅ ✅ ✅  Assertion Passed: ${actual} === ${expected}`);
 };
 
 
-const tail = function (array) {
-  let tailArray = [];
-  tailArray = array.slice(1);
-  return tailArray;
+const tail = function(array) {
+  return array.slice(1);
 };
 
 
