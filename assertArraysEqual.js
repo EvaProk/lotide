741801837ca2da0f914b0d0fd976c331aeb72134
Eva Prokhorenko <evaprokhorenko@gmail.com>
2021-08-17 23:33:51 +0000

    adding assertArraysEqual.js function

---
+++ assertArraysEqual.js
@@ -0,0 +1,20 @@
+const assertArraysEqual = function (actual, expected) {
+  if (actual.length !== expected.length) {
+    console.log(`⛔️ ⛔️ ⛔️Assertion Failed: ${actual} !== ${expected}`);
+    return;
+  }
+
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      console.log(`⛔️ ⛔️ ⛔️Assertion Failed: ${actual} !== ${expected}`);
+      return;
+    }
+  }
+  console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
+  return;
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);;
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
