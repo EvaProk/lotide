3ffaa7a60fa4a094b38299b144371bb14c001e18
Eva Prokhorenko <evaprokhorenko@gmail.com>
2021-08-19 03:49:09 +0000

    Correct mistakes in eqObjects.js

--- eqObjects.js
+++ eqObjects.js
@@ -1,4 +1,4 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -6,7 +6,7 @@ const assertEqual = function (actual, expected) {
   }
 };
 
-const eqArrays = function (actual, expected) {
+const eqArrays = function(actual, expected) {
   if (actual.length !== expected.length) {
     return false;
   }
@@ -20,7 +20,7 @@ const eqArrays = function (actual, expected) {
 };
 
 
-const eqObjects = function (object1, object2) {
+const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
   }
@@ -28,9 +28,11 @@ const eqObjects = function (object1, object2) {
 
   for (let key of keys) {
     if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      eqArrays(object1[key], object2[key]);
-    } else if (object1[key] != object2[key]) {
-      return false;
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      } else if (object1[key] !== object2[key]) {
+        return false;
+      }
     }
   }
   return true;
