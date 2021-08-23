f2c2a773dfa1a4ad35413a420d542119b60264b4
Eva Prokhorenko <evaprokhorenko@gmail.com>
2021-08-23 22:10:00 +0000

    add Chai assertion tests

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,11 +1,31 @@
-const assertArraysEqual = require('../assertArraysEqual');
-const middle = require('../middle');
+const assert = require('chai').assert;
+const middle  = require('../middle');
 
-// All assertion pass
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle(['eva', 'roma', 'valik', 'volodya']), ['roma', 'valik']);
+describe("Should return middle element in array(if the array has even number of elements , return 2 middle elem.) ", () => {
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); 
+  });
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]); 
+  });
+
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]); 
+  });
+
+  it("returns empty array  for [1]", () => {
+    assert.deepEqual(middle([1]), []); 
+  });
+
+  it("returns empty array  for [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), []); 
+  });
+
+  it("returns ['roma', 'valik']  for ['eva', 'roma', 'valik', 'volodya']", () => {
+    assert.deepEqual(middle(['eva', 'roma', 'valik', 'volodya']), ['roma', 'valik']); 
+  });
+
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,15 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
+describe("Should return the array without the first element ", () => {
+  it("returns [5, 5] for [7, 5, 5]", () => {
+    assert.deepEqual(tail([7, 5, 5]), [5, 5]);
+  });
+  it("returns [4, 5, 3] for [3, 4, 5, 3]", () => {
+    assert.deepEqual(tail([3, 4, 5, 3]), [4, 5, 3]); 
+  });
+  it("returns [Roma, Valik, tanya, Marik] for [Roma, Valik, tanya, Marik", () => {
+    assert.deepEqual(tail(['Eva', "Roma", "Valik", "tanya", "Marik"]), ["Roma", "Valik", "tanya", "Marik"]); 
+  });
 
-assertArraysEqual(tail([7, 5, 5]), [5, 5]); // pass
-assertArraysEqual(tail([3, 4, 5, 3]), [4, 5, 3]); // pass
-assertArraysEqual(tail(['Eva', "Roma", "Valik", "tanya", "Marik"]), ["Roma", "Valik", "tanya", "Marik"]); //pass
-assertArraysEqual(tail(['Eva', "Roma", "Valik", "tanya", "Marik"]), ["tanya", "Marik"]); //fail
+});
