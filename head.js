ed63e22d8550f0e143d0c94cb1e289e67925a0ac
Eva Prokhorenko <evaprokhorenko@gmail.com>
2021-08-23 19:30:35 +0000

    add exports modules and separate test cases into the test/ folder

--- head.js
+++ head.js
@@ -1,15 +1,10 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`⛔️ ⛔️ ⛔️Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// const assertEqual = require('./assertEqual');
+
 
 const head = function(array) {
   let firstElement = array[0];
   return firstElement;
 };
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["HelloEva", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
+
