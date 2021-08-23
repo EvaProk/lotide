ca1c183b13323480c9de58d77d153b9a65c44219
Eva Prokhorenko <evaprokhorenko@gmail.com>
2021-08-23 21:06:54 +0000

    Refactor test/middleTest.js  middle.js and add test files

--- middle.js
+++ middle.js
@@ -1,35 +1,4 @@
-const assertArraysEqual = function (actual, expected) {
-  if (actual.length !== expected.length) {
-    console.log(`⛔️ ⛔️ ⛔️Assertion Failed: ${actual} !== ${expected}`);
-    return;
-  }
-
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      console.log(`⛔️ ⛔️ ⛔️Assertion Failed: ${actual} !== ${expected}`);
-      return;
-    }
-  }
-  console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
-  return;
-};
-
-
-const eqArrays = function (actual, expected) {
-  if (actual.length !== expected.length) {
-    return false;
-  }
-
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-
-const middle = function (array) {
+const middle = function(array) {
   let resultArray = [];
   let midIndex = Math.floor(array.length / 2);
   if (array.length <= 2) {
@@ -37,29 +6,16 @@ const middle = function (array) {
   }
 
   if (array.length % 2 === 0) {
-    resultArray.push(array[midIndex - 1])
+    resultArray.push(array[midIndex - 1]);
     resultArray.push(array[midIndex]);
   } else if (array.length % 2 !== 0) {
     resultArray.push(array[midIndex]);
   }
   return resultArray;
-}
-
-console.log(middle([1, 2, 3, 4]))  // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // =>[3, 4]
-console.log(middle([1, 2, 3]))// => [2]
-console.log(middle([1, 2, 3, 4, 5]))// => [3]
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
+};
 
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3])
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle(['eva', 'roma', 'valik', 'volodya']), ['roma', 'valik']);
+module.exports = middle;
 
 
 
