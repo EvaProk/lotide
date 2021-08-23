c654dcd7072e4c1456a169a25f8d66d8b33652c8
Eva Prokhorenko <evaprokhorenko@gmail.com>
2021-08-23 20:26:13 +0000

    Create export modules and separate test files for assertArraysEqual.js tail.js

--- tail.js
+++ tail.js
@@ -1,25 +1,5 @@
-
-const assertEqual = function(actual, expected) {
-  if (actual.length !== expected.length) {
-    console.log(`⛔️ ⛔️ ⛔️ Assertion Failed: ${actual} !== ${expected}`);
-    return;
-  }
-
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      console.log(`$⛔️ ⛔️ ⛔️Assertion Failed: ${actual} !== ${expected}`);
-      return;
-    }
-  }
-
-  console.log(`✅ ✅ ✅  Assertion Passed: ${actual} === ${expected}`);
-};
-
-
 const tail = function(array) {
   return array.slice(1);
 };
 
-
-const result = tail([7, 5, 5]);
-assertEqual(result, [5, 5]);
+module.exports = tail;
